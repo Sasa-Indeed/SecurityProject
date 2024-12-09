2193534d73f131d2a9d141233883d16be6ccc08e
Norhan-salem <norhansalem581@gmail.com>
2024-12-10 01:09:38 +0200

    added rsa router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from .api.endpoints import authentication
+from .api.endpoints.rsa_endpoints import router as rsa_router
 
 app = FastAPI()
 
 app.include_router(authentication.router, prefix="/auth", tags=["Authentication"])
+app.include_router(rsa_router, prefix="/rsa", tags=["RSA Operations"])
+
