afa04b8f7181bcd206a3fe32f92554372e414f33
Norhan-salem <norhansalem581@gmail.com>
2024-12-10 01:07:15 +0200

    added rsa endpoints

--- backend/app/api/endpoints/rsa_endpoints.py
+++ backend/app/api/endpoints/rsa_endpoints.py
@@ -1,9 +1,27 @@
-from fastapi import APIRouter
-from backend.app.tests.rsa_encryption_test import rsa_service
+from fastapi import APIRouter, HTTPException
+from backend.app.models.encrypt_request import EncryptRequest
+from backend.app.models.decrypt_request import DecryptRequest
+from backend.app.core.rsa_service import RSACipher
+
+rsa_service = RSACipher(
+    private_key_path="backend/app/core/private_key.pem",
+    public_key_path="backend/app/core/public_key.pem"
+)
 
 router = APIRouter()
 
-@router.post("/encrypt-with-rsa/")
-async def encrypt_message(message: str):
-    encrypted_message = rsa_service.encrypt_message(message)
-    return {"encrypted_message": encrypted_message}
+@router.post("/encrypt")
+async def encrypt(data: EncryptRequest):
+    try:
+        ciphertext = rsa_service.encrypt(data.plaintext)
+        return {"ciphertext": ciphertext}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.post("/decrypt")
+async def decrypt(data: DecryptRequest):
+    try:
+        plaintext = rsa_service.decrypt(data.ciphertext)
+        return {"plaintext": plaintext}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
