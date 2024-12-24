7e4d0d74e445303ce0a446d433976b2d384134ab
Norhan-salem <norhansalem581@gmail.com>
2024-12-24 20:04:38 +0200

    added cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,7 @@ from .api.endpoints.rsa_endpoints import router as rsa_router
 from .api.endpoints.block_cipher_endpoints import router as aes_router
 from .api.endpoints.email_endpoints import router as email_router
 from .api.endpoints.key_management import router as key_router
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -13,3 +14,10 @@ app.include_router(aes_router, prefix="/aes", tags=["AES Operations"])
 app.include_router(email_router, prefix="/email", tags=["Email Operations"])
 app.include_router(key_router, prefix="/keys", tags=["Key Management"])
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
