1b4937aba4c0308555ed885e61caee23de9b4fa7
Norhan-salem <norhansalem581@gmail.com>
2024-12-25 02:38:51 +0200

    wrapped with auth

--- frontend/secure-communication-suite/src/index.js
+++ frontend/secure-communication-suite/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import { AuthProvider } from './context/AuthContext.jsx';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <AuthProvider>
   <App />
+</AuthProvider>,
 );
 
