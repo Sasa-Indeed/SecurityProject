fe8d9eb6b942fb7f5e89f78b2595588a67d028fa
Norhan-salem <norhansalem581@gmail.com>
2024-12-25 02:36:17 +0200

    integrated auth

--- frontend/secure-communication-suite/src/pages/authPage.jsx
+++ frontend/secure-communication-suite/src/pages/authPage.jsx
@@ -3,16 +3,18 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import AuthCard from "../components/Auth/AuthCard";
 import { login, signup } from "../services/api";
-import { validateFields } from "../utils/validators"; 
 import "../styles/auth.css";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 const AuthPage = () => {
   const [isLogin, setIsLogin] = useState(true);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  
+  const navigate = useNavigate();
+  const { setUserEmail } = useAuth();
+
   useEffect(() => {
     AOS.init({
       duration: 1000,
@@ -24,22 +26,25 @@ const AuthPage = () => {
     setIsLogin((prev) => !prev);
   };
 
-  const handleLogin = async (event) => {
-    event.preventDefault();
+  const handleLogin = async () => {
     try {
       const data = await login(email, password);
+      setUserEmail(email);
       console.log("Login success:", data);
+      navigate("/dashboard");
     } catch (error) {
       console.error("Login error:", error.message);
       alert(`Error: ${error.message}`);
     }
   };
 
-  const handleSignup = async (event) => {
-    event.preventDefault();
+  const handleSignup = async () => {
     try {
+      console.log("Signup with:", { email, password });
       const data = await signup(email, password);
+      setUserEmail(email);
       console.log("Signup success:", data);
+      navigate("/dashboard");
     } catch (error) {
       console.error("Signup error:", error.message);
       alert(`Error: ${error.message}`);
@@ -50,6 +55,12 @@ const AuthPage = () => {
     <div className="auth-page">
       <AuthCard
         isLogin={isLogin}
+        email={email}
+        setEmail={setEmail}
+        password={password}
+        setPassword={setPassword}
+        confirmPassword={confirmPassword}
+        setConfirmPassword={setConfirmPassword}
         handleLogin={handleLogin}
         handleSignup={handleSignup}
         toggleForm={toggleForm}
